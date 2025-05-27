3ffa47c46deaae4ea5e873c6fbb302e33401dde3
webandrs <d3vv.proo@gmail.com>
2025-05-27 15:47:18 +0700

    feat: Added toggle burger menu(hidden-menu)

--- src/js/common/common.js
+++ src/js/common/common.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
 	'use strict'
+
+	toggleBurgerMenu()
 })
+
+const toggleBurgerMenu = () => {
+	const header = document.querySelector('.header')
+	const burgerButton = header?.querySelector('.burger-button')
+	const burgerMenu = header?.querySelector('.hidden-menu')
+
+	if(!header || !burgerButton || !burgerMenu) return
+
+	burgerButton.addEventListener('click', () => {
+		header.classList.toggle('opened')
+	})
+}
