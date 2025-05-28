70f0f2f0dc2d2ed37def54a6bf645d0a9ea135fb
webandrs <d3vv.proo@gmail.com>
2025-05-28 09:54:35 +0700

    feat: Added custom scrollbar.

---
+++ src/js/simpleBar.js
@@ -0,0 +1,17 @@
+import SimpleBar from 'simplebar'
+
+document.addEventListener('DOMContentLoaded', () => {
+	'use strict'
+
+	initSimpleBar()
+})
+
+const initSimpleBar = () => {
+	const scrollableElements = document.querySelectorAll('[data-simplebar]')
+	scrollableElements.forEach(element => {
+		new SimpleBar(element, {
+			autoHide: false,
+			scrollbarMinSize: 10
+		})
+	})
+}
